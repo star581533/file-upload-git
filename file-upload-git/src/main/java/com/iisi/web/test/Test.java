314e9fe5b75e42bfa3acb1a1b2f585f0a7615d6c
1104611 <1104611@1104611NB01.iead.local>
2015-10-06 11:53:24 +0800

    Update

--- file-upload-git/src/main/java/com/iisi/web/test/Test.java
+++ file-upload-git/src/main/java/com/iisi/web/test/Test.java
@@ -1,12 +1,27 @@
 package com.iisi.web.test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Test {
 	public static void main(String args[]){
 		new Test().getOsName();
+		boolean bool = new Test().verifyBirthYymmdd("");
+		System.out.println(bool);
 	}
 	
 	public void getOsName(){
 		String osName = System.getProperty("os.name");
 		System.out.println(osName);
 	}
+	
+    public static boolean verifyBirthYymmdd(String yyymmdd) {
+        boolean bool = false;
+        Pattern pattern = Pattern.compile("[0-9]*");
+        Matcher isNum = pattern.matcher(yyymmdd);
+        if (isNum.matches()) {
+            bool = true;
+        }
+        return bool;
+    }
 }
